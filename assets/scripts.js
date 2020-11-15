ab6e0aeaa3f7a8deaed5e880afa93e0753a30d73
mooselumph <robert.raynor@gmail.com>
2020-11-15 14:10:20 -0800

    rebuilding site Sun, Nov 15, 2020  2:10:20 PM

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,3 +12,49 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+// Loop throw tag classes
+
+document.addEventListener('KatexRendered',function(){
+
+var eq_labels = {};
+var eq_num = 1;
+
+var tags = document.getElementsByClassName("tag");
+
+for (i = 0; i < tags.length; i++) {
+    var span = tags[i].children[1].children[1]
+    
+    if (span.hasChildNodes()){
+        
+        var label = span.children[0].id;
+        
+        var child = document.createElement('a');
+        child.href = label;
+        span.appendChild(child);
+        
+        eq_labels[label] = eq_num;
+    }
+    
+    span.innerHTML += eq_num.toString();
+    eq_num += 1;
+}
+
+var refs = document.getElementsByClassName("eq-ref");
+
+for (i = 0; i < refs.length; i++) {
+    var ref = refs[i]
+    
+    ref.classList.forEach(function(label){
+        if (eq_labels.hasOwnProperty(label)){
+
+            var child = document.createElement('a');
+            child.href = "#" + label;
+            child.innerHTML +=  "(" + eq_labels[label].toString()  +")";
+            ref.appendChild(child);
+
+        }
+    });
+}
+
+});
